48e5ce12926c05fdeb9a7edbcbc809277ecb27c1
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-10-18 19:00:15 +0700

    Memfungsikan ic favorit

--- src/components/pages/Watch/index.jsx
+++ src/components/pages/Watch/index.jsx
@@ -4,9 +4,30 @@ import outletInit from '../../../js/components/pages/outlet';
 import '../../../css/component/pages/watch.css';
 
 export default class WatchClass extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLiked: false,
+    };
+
+    this.ic_white_url = '/images/icon/heart_white.png';
+    this.ic_red_url = '/images/icon/heart_red.png';
+
+    this.onIcHeartClicked = this.onIcHeartClicked.bind(this);
+  }
+
   componentDidMount() {
     outletInit('watch');
     this.props.onOutletChange();
+  }
+
+  onIcHeartClicked() {
+    this.setState(
+      (prevState) => {
+        return { isLiked: !prevState.isLiked }
+      }
+    );
   }
 
   render() {
@@ -20,7 +41,13 @@ export default class WatchClass extends Component {
           allowFullScreen></iframe>
         <section id="titleSection">
           <p>Webinar Seri COVID-19 Waspada Omicron, Kenali dan Kendalikan Penyakit Penyerta KOMORBID</p>
-          <img src="/images/icon/heart_white.png" alt="" />
+          <img
+            src={
+              this.state.isLiked ? this.ic_red_url : this.ic_white_url
+            }
+            onClick={ () => this.onIcHeartClicked() }
+            alt="icon heart"
+            />
         </section>
       </div>
     )
