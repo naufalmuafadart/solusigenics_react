a326bf882f8772bf31307527ba1431eccb1f5079
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-10-28 06:41:33 +0700

    Menampilkan video rekomendasi

--- src/components/pages/HomeIndex/index.jsx
+++ src/components/pages/HomeIndex/index.jsx
@@ -3,48 +3,63 @@ import OutletHeading2 from '../../atoms/OutletHeading2';
 import LoadingScreen from '../../molecules/LoadingScreen';
 import VideoCardList from "../../organisms/VideoCardList";
 
+import {
+  fetchRequestToHapiWithAuth,
+  fetchRequestToHapi,
+  fetchRequestToFlask,
+} from '../../../js/common';
+
 import outletInit from '../../../js/components/pages/outlet';
 import '../../../css/component/pages/home_index.css';
 
 class HomeIndexClass extends Component {
   constructor(props) {
     super(props);
-    this.getVideoRecommendation = this.getVideoRecommendation.bind(this);
-
+    
     this.state = {
       videos: [],
+      is_finish_mounted: false,
     };
+
+    this.getVideoRecommendation = this.getVideoRecommendation.bind(this);
   }
 
-  async getVideoRecommendation(disease) {
-    let response = await fetch(`${import.meta.env.VITE_FLASK_HOST}/get_video_by_disease?query=${disease}`);
-    if (response.status === 200) {
-      let data = await response.text();
-      data = JSON.parse(data);
-      return data["videos"];
-    }
-    else {
-      return [];
-    }
+  async getVideoRecommendation() {
+    let response = await fetchRequestToHapiWithAuth('/favorites', 'GET', null);
+    let data = response.data;
+    const arr_video_id = data.map((video) => ({ id: video.video_id }));
+
+    let body = {
+      video_ids: arr_video_id,
+    };
+    response = await fetchRequestToHapi('/videos/get', 'POST', body);
+    data = response.data;
+    
+    body = data.map((video) => ({ id: video.actual_id, source: video.source, }));
+    response = await fetchRequestToFlask('/get_video_recommendation', 'POST', body);
+    data = JSON.parse(response);
+    return data.videos;
   }
 
   async componentDidMount() {
     outletInit('HomeIndex', 'Beranda');
     this.props.onAsideButtonClicked(0);
     this.props.onOutletChange();
-    const videos = await this.getVideoRecommendation('Tipes');
-
-    const LoadingScreen = document.getElementById('loading-screen');
-    LoadingScreen.style.display = 'none';
+    const videos = await this.getVideoRecommendation();
     
-    this.setState({ videos });
+    this.setState({
+      videos,
+      is_finish_mounted: true,
+    });
   }
 
   render() {
     return (
       <div id="HomeIndex">
         <OutletHeading2 text="Rekomendasi Untuk Anda" />
-        <LoadingScreen />
+        {
+          (!this.state.is_finish_mounted) ? <LoadingScreen /> : null
+        }
         <VideoCardList videos={this.state.videos} />
       </div>
     );
