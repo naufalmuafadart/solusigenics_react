6ebea5a714022f6e5021405b7a336088aaca1165
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-10-31 18:16:56 +0700

    Memperbaiki style video card

--- src/components/molecules/VideoCard/index.jsx
+++ src/components/molecules/VideoCard/index.jsx
@@ -6,15 +6,17 @@ import "../../../css/component/molecules/VideoCard.css";
 export default function VideoCard(props) {
   const { id, title, thumbnail, source } = props;
   return (
-    <Link
-      to={`/tonton?id=${id}&source=${source}`}
-      className="video"
-      style={{ textDecoration: "none" }}
-      >
-      <figure>
-        <img src={thumbnail} />
-        <figcaption>{ title }</figcaption>
-      </figure>
-    </Link>
+    <div className="video_card">
+      <Link
+        to={`/tonton?id=${id}&source=${source}`}
+        className="video"
+        style={{ textDecoration: "none" }}
+        >
+        <figure>
+          <img src={thumbnail} />
+          <figcaption>{ title }</figcaption>
+        </figure>
+      </Link>
+    </div>
   );
 }
