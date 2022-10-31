ffbb67317254c82a1ae265fd230040bd8926ca9f
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2022-10-31 10:06:43 +0700

    Mengatur video di class HistoryAndFavorite

--- src/components/organisms/VideoCardList2/index.jsx
+++ src/components/organisms/VideoCardList2/index.jsx
@@ -1,103 +1,28 @@
 import VideoCard2 from '../../molecules/VideoCard2';
 import React, { useEffect, useState } from 'react';
-import {
-	fetchRequestToHapiWithAuth,
-	fetchRequestToHapi,
-	fetchRequestToFlask
-} from '../../../js/common';
 
 const VideoCardList2 = ( props ) => {
 	const {
-		url,
-		date,
 		onDeleteVideo,
-		setDataFromHapi,
-		setDataFromFlask,
-		setIsOnLoadVideo,
-    setStateHeading,
-    is_first_time_load_video,
     data_from_flask,
-    props_heading,
-    state_heading,
 	} = props;
-	const [videos, setVideos] = useState("");
-
-	useEffect(() => {
-		const fetchData = async () => {
-			setIsOnLoadVideo(true);
-      setStateHeading();
-      if (is_first_time_load_video || props_heading != state_heading) {
-        let result = await fetchRequestToHapiWithAuth(url, 'GET', null);
-
-        const videos = result.data;
-
-        const arr_video_id = videos.map((video) => video.video_id);
-        setVideos(arr_video_id);
-        
-        if (arr_video_id.length != 0) {
-          let raw = {
-            "video_ids": arr_video_id.map((video) => ({ id: video })),
-          };
-
-          result = await fetchRequestToHapi('/videos/get', 'POST', raw);
-          let data_from_hapi = result['data'];
-          
-          raw = data_from_hapi.map(
-            (video) => ({
-              id: video.actual_id,
-              source: video.source,
-            })
-          );
-          
-          let data = await fetchRequestToFlask('/get_videos_detail', 'POST', raw);
-          data = JSON.parse(data);
-          data = data.videos;
-          setDataFromHapi(data_from_hapi);
-          setDataFromFlask(data);
-          setVideos(data);
-        }
-        else {
-          setVideos([]);
-        }
-      }
-      else {
-        setVideos(data_from_flask);
-      }
-			setIsOnLoadVideo(false);
-    };
-
-    fetchData();
-	}, [
-    url,
-    date,
-    is_first_time_load_video,
-    data_from_flask,
-    props_heading,
-    state_heading,
-  ]);
 
 	return (
 		<>
-			{
-				Array.isArray(videos) && videos.length > 0 ?
-					videos[0].disease !== null ? 
-						videos.map(
-							(video) => (
-								<VideoCard2
-									key={video.id}
-									id={video.id}
-									source={video.source}
-									title={video.title}
-									thumbnail={video.thumbnail}
-									onDeleteVideo={onDeleteVideo}
-									/>
-							)
-						)
-					:
-						null
-				:
-					null
-			}
+      {
+        data_from_flask.map(
+          (video) => (
+            <VideoCard2
+              key={video.id}
+              id={video.id}
+              source={video.source}
+              title={video.title}
+              thumbnail={video.thumbnail}
+              onDeleteVideo={onDeleteVideo}
+              />
+          )
+        )
+      }
 		</>
 	);
 };
--- src/components/pages/HistoryAndFavorite/index.jsx
+++ src/components/pages/HistoryAndFavorite/index.jsx
@@ -7,12 +7,31 @@ import { hide as hideNavbar } from '../../../js/components/organisms/navbar';
 import outletInit from "../../../js/components/pages/outlet";
 import {
   checkIsLoggedIn,
-  fetchRequestToHapiWithAuth
+  fetchRequestToHapiWithAuth,
+  fetchRequestToHapi,
+  fetchRequestToFlask,
 } from '../../../js/common';
 
 import VideoCardList2 from '../../organisms/VideoCardList2';
 
 import '../../../css/component/pages/history_and_favorite.css';
+
+const HeadingPropsChangeDetector = (props) => {
+  const {props_heading, state_heading, date, onPropsChange } = props;
+  useEffect(() => {
+    const checkPropsChange = async () => {
+      if (props_heading != state_heading) {
+        await onPropsChange();
+      }
+    };
+    checkPropsChange();
+  }, [
+    props_heading,
+    state_heading,
+    date,
+  ]);
+  return (null);
+};
 
 class HistoryAndFavoriteClass extends Component {
   constructor(props) {
@@ -29,17 +48,20 @@ class HistoryAndFavoriteClass extends Component {
       date: +new Date(),
     };
 
+    this.alertNoVideo = `Tidak ada ${this.props.heading.toLowerCase()}`;
+
     this.onDeleteVideo = this.onDeleteVideo.bind(this);
-    this.setDataFromHapi = this.setDataFromHapi.bind(this);
-    this.setDataFromFlask = this.setDataFromFlask.bind(this);
-    this.setIsOnLoadVideo = this.setIsOnLoadVideo.bind(this);
     this.setStateHeading = this.setStateHeading.bind(this);
+    this.loadVideo = this.loadVideo.bind(this);
+    this.onPropsChange = this.onPropsChange.bind(this);
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     outletInit('HnFContainer', this.props.heading);
     this.props.onAsideButtonClicked(0);
     this.props.onOutletChange();
+
+    await this.loadVideo();
 
     this.setState({
       is_finish_mounted: true,
@@ -52,26 +74,50 @@ class HistoryAndFavoriteClass extends Component {
     checkIsLoggedIn();
   }
 
-  setDataFromHapi(data) {
+  async loadVideo() {
     this.setState({
-      is_first_time_load_video: false,
-      data_from_hapi: data,
+      is_on_load_video : true,
+      data_from_flask: [],
+      data_from_hapi: [],
     });
-  }
+    let url = this.props.heading.includes('History') ? '/histories' : '/favorites';
+    let result = await fetchRequestToHapiWithAuth(url, 'GET', null);
+    const videos = result.data;
 
-  setDataFromFlask(data) {
-    this.setState({
-      is_first_time_load_video: false,
-      data_from_flask: data,
-    });
-  }
+    const arr_video_id = videos.map((video) => video.video_id);
 
-  setIsOnLoadVideo(is_on_load_video) {
-    this.setState({ is_on_load_video });
+    if (arr_video_id.length != 0) {
+      let raw = {
+        "video_ids": arr_video_id.map((video) => ({ id: video })),
+      };
+
+      result = await fetchRequestToHapi('/videos/get', 'POST', raw);
+      let data_from_hapi = result['data'];
+      
+      raw = data_from_hapi.map(
+        (video) => ({
+          id: video.actual_id,
+          source: video.source,
+        })
+      );
+      
+      let data_from_flask = await fetchRequestToFlask('/get_videos_detail', 'POST', raw);
+      data_from_flask = JSON.parse(data_from_flask);
+      data_from_flask = data_from_flask.videos;
+      
+      this.setState({ data_from_hapi, data_from_flask, });
+    }
+    this.setState({ is_on_load_video : false });
   }
 
   setStateHeading() {
     this.setState({ heading : this.props.heading, });
+  }
+
+  async onPropsChange() {
+    this.alertNoVideo = `Tidak ada ${this.props.heading.toLowerCase()}`;
+    this.setStateHeading();
+    await this.loadVideo();
   }
 
   async onDeleteVideo(id) {
@@ -91,20 +137,23 @@ class HistoryAndFavoriteClass extends Component {
       <div id="HnFContainer">
         <OutletHeading1 text={this.props.heading} />
         {
+          (this.state.is_finish_mounted && this.state.data_from_flask.length == 0) ?
+            <h2 className="alert">{this.alertNoVideo}</h2>
+          :
+            null
+        }
+        {
           (this.state.is_on_load_video) ? <LoadingScreen /> : null
         }
         <VideoCardList2
-          url={this.props.heading.includes('History') ? '/histories': '/favorites'}
           onDeleteVideo={this.onDeleteVideo}
-          date={this.state.date}
-          setDataFromHapi={this.setDataFromHapi}
-          setDataFromFlask={this.setDataFromFlask}
-          setIsOnLoadVideo={this.setIsOnLoadVideo}
-          setStateHeading={this.setStateHeading}
-          is_first_time_load_video={this.state.is_first_time_load_video}
           data_from_flask={this.state.data_from_flask}
+          />
+        <HeadingPropsChangeDetector
           props_heading={this.props.heading}
           state_heading={this.state.heading}
+          date={this.state.date}
+          onPropsChange={this.onPropsChange}
           />
       </div>
     )
